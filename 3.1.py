ffe84137a7c47616e5d7697dd215dfc31b8d6770
bit-x <bitxzq@gmail.com>
2022-08-12 01:04:23 +0400

    re

--- 3.1.py
+++ 3.1.py
@@ -22,39 +22,39 @@
 # f.close()
 
 
-# a = []
-# b = []
-# c = []
-# d = []
-# e = []
-# f = open('1.txt')
-# for line in f:
-#     a.append(line.split())
-# f.close()
-# for i in a:
-#     for j in i:
-#         b.append(j)
-#
-# for i in b:
-#     if i == '-':
-#         b.remove(i)
-#
-# for j in b:
-#     for i in j:
-#         if i == '1' or i=='2' or i=='3' or i=='4' or i=='5' or i=='6' or i=='7' or i=='8' or i=='9' or i=='0':
-#             j = float(j)
-#             j = j- (j / 100 * 13)
-#             j = round(j)
-#             break
-#     c.append(j)
-# for i in c:
-#     if type(i) == str:
-#         d.append(i.upper())
-#     else:
-#         e.append(i)
-# a = (dict(zip(d, e)))
-# print(a)
-# for i, j in a.items():
-#     if j<500000:
-#         print(i)
-#         print(j)
+a = []
+b = []
+c = []
+d = []
+e = []
+f = open('1.txt')
+for line in f:
+    a.append(line.split())
+f.close()
+for i in a:
+    for j in i:
+        b.append(j)
+
+for i in b:
+    if i == '-':
+        b.remove(i)
+
+for j in b:
+    for i in j:
+        if i == '1' or i=='2' or i=='3' or i=='4' or i=='5' or i=='6' or i=='7' or i=='8' or i=='9' or i=='0':
+            j = float(j)
+            j = j- (j / 100 * 13)
+            j = round(j)
+            break
+    c.append(j)
+for i in c:
+    if type(i) == str:
+        d.append(i.upper())
+    else:
+        e.append(i)
+a = (dict(zip(d, e)))
+print(a)
+for i, j in a.items():
+    if j<500000:
+        print(i)
+        print(j)
---
+++ lesson 4.py
@@ -0,0 +1,18 @@
+# x = [1, 2, 3]
+# def test (some_list):
+#     y = input()
+#     some_list.append(y)
+#
+# test(x)
+# print(x)
+#
+# age = int(input('age= '))
+# print ('Welcome' if age>=18 else 'No access')
+
+# age = -1
+# while age == -1:
+#     try:
+#         age = int(input('age= '))
+#     except ValueError:
+#         print('No age')
+# print(age)
